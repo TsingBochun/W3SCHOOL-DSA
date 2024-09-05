f6aff2c7e0d993ed83a781b0c80716d33f7d0bb8
TsingBochun <tsingbochun@gmail.com>
2024-09-05 21:01:18 +0800

    selection

--- bubble.py
+++ bubble.py
@@ -1,14 +1,14 @@
 # Bubble Sort Implementation
-my_array = [7, 3, 9, 12, 11]
+
+my_array = [64, 34, 25, 5, 22, 11, 90, 12]
 
 n = len(my_array)
 for i in range(n - 1):
-    swapped = False
-    for j in range(n - i -1):
-        if my_array[j] > my_array[j + 1]:
-            my_array[j], my_array[j + 1] = my_array[j + 1], my_array[j]
-            swapped = True
-    if not swapped:
-        break
+    min_index = i
+    for j in range(i + 1, n):
+        if my_array[j] < my_array[min_index]:
+            min_index = j
+    min_value = my_array.pop(min_index)
+    my_array.insert(i, min_value)
 
 print("Sorted array:", my_array)
