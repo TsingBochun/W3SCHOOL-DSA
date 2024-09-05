f2c12d8e0ba3cec9a655fd2959332fe009162e5f
TsingBochun <tsingbochun@gmail.com>
2024-09-05 20:25:54 +0800

    min val

--- DSA_Arrays.py
+++ DSA_Arrays.py
@@ -1,3 +1,10 @@
 
 my_array = [7, 12, 9, 4, 11]
-print( my_array[0] )
+# print( my_array[0] )
+minVal = my_array[0]  # Step 1
+
+for i in my_array:    # Step 2
+    if i < minVal:    # Step 3
+        minVal = i
+
+print('Lowest value: ', minVal)    # Step 4
