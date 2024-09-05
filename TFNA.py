212460f9ae354290ed975ffe8610a66aaa1e7d3d
TsingBochun <tsingbochun@gmail.com>
2024-09-05 17:43:34 +0800

    Implementation Using Recursion

--- TFNA.py
+++ TFNA.py
@@ -1,13 +1,19 @@
-# Implementation Using a For Loop
+# Implementation Using Recursion
 
-prev2 = 0
-prev1 = 1
+print(0)
+print(1)
+count = 2
 
-print(prev2)
-print(prev1)
-
-for fibo in range(18):
-    newFibo = prev1 + prev2
-    print(newFibo)
-    prev2 = prev1
-    prev1 = newFibo
+def fibonacci(prev1, prev2):
+    global count
+    if count <= 19:
+        newFibo = prev1 + prev2
+        print(newFibo)
+        prev2 = prev1
+        prev1 = newFibo
+        count += 1
+        fibonacci(prev1, prev2)
+    else:
+        return
+    
+fibonacci(1, 0)
