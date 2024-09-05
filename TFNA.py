892a01a0afc6457d9bc5341a48e78125596478f7
TsingBochun <tsingbochun@gmail.com>
2024-09-05 17:45:16 +0800

    Finding The nth Fibonacci Number Using Recursion

--- TFNA.py
+++ TFNA.py
@@ -1,19 +1,9 @@
-# Implementation Using Recursion
+# Finding The nth Fibonacci Number Using Recursion
 
-print(0)
-print(1)
-count = 2
-
-def fibonacci(prev1, prev2):
-    global count
-    if count <= 19:
-        newFibo = prev1 + prev2
-        print(newFibo)
-        prev2 = prev1
-        prev1 = newFibo
-        count += 1
-        fibonacci(prev1, prev2)
+def F(n):
+    if n <= 1:
+        return n
     else:
-        return
+        return F(n - 1) + F(n - 2)
     
-fibonacci(1, 0)
+print(F(19))
